ce0731d86bd8253db731caf88e0dd288e93932fa
lucianodev <mirendaluciano@gmail.com>
2024-05-01 19:40:59 -0300

    practica3 ejercicio 10

---
+++ src/practica3/ejercicio10/Ejercicio10.java
@@ -0,0 +1,27 @@
+package practica3.ejercicio10;
+
+import practica3.GeneralTree;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+
+        GeneralTree<Integer> arbol = new GeneralTree<>();
+
+        arbol.setData(1);
+        arbol.addChild(new GeneralTree<>(1));
+        arbol.addChild(new GeneralTree<>(0));
+        arbol.getChildren().get(0).addChild(new GeneralTree<>(1));
+        arbol.getChildren().get(0).addChild(new GeneralTree<>(1));
+        arbol.getChildren().get(0).getChildren().get(1).addChild(new GeneralTree<>(1));
+
+
+        arbol.getChildren().get(0).getChildren().get(0).addChild(new GeneralTree<>(1));
+
+        arbol.getChildren().get(1).addChild(new GeneralTree<>(1));
+        arbol.getChildren().get(1).getChildren().get(0).addChild(new GeneralTree<>(1));
+        arbol.getChildren().get(1).getChildren().get(0).getChildren().get(0).addChild(new GeneralTree<>(1));
+
+        System.out.println(ParcialArboles.resolver(arbol));
+    }
+}
---
+++ src/practica3/ejercicio10/ParcialArboles.java
@@ -0,0 +1,83 @@
+package practica3.ejercicio10;
+
+import practica3.GeneralTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ParcialArboles {
+
+    private static class Valores {
+        int calculo;
+        int max;
+
+        Valores(){
+            max = -1;
+            calculo = 0;
+        }
+
+    }
+
+
+    public static List<Integer> resolver(GeneralTree<Integer> arbol) {
+
+        List<Integer> lista = new ArrayList<>();
+        List<Integer> resultado = new ArrayList<>();
+
+        Valores valores = new Valores();
+        int nivel = 0;
+
+        resolver(arbol, lista, resultado, valores,nivel);
+
+        return resultado;
+    }
+
+    private static void resolver(GeneralTree<Integer> arbol, List<Integer> lista,List<Integer> listaMax, Valores valores,int nivel) {
+
+        if (arbol.isLeaf()) {
+            if (arbol.getData() == 1) {
+                lista.add(1);
+            }
+
+            valores.calculo = valores.calculo + (nivel * arbol.getData());
+            System.out.println("[hoja] nivel: "+nivel+ " calculo: "+valores.calculo);
+
+
+        } else {
+
+            for (GeneralTree<Integer> child : arbol.getChildren()) {
+
+                if (arbol.getData() == 1) {
+                    lista.add(1);
+                }
+
+                valores.calculo = valores.calculo + (nivel * arbol.getData());
+
+                System.out.println("[entra] nivel: "+nivel+ " calculo: "+valores.calculo+" max: "+valores.max);
+
+                int calculoAnterior = valores.calculo;
+
+                resolver(child, lista,listaMax, valores,nivel+1);
+
+                System.out.println("[sale] nivel: "+nivel+ " calculo: "+valores.calculo+" max: "+valores.max);
+
+                if (valores.calculo > valores.max) {
+                    listaMax.clear();
+                    valores.max = valores.calculo;
+                    for (int num : lista) {
+                        listaMax.add(num);
+                    }
+                }
+                if(lista.size()>0){
+                    lista.remove(lista.size()-1);
+                }
+
+                valores.calculo = calculoAnterior;
+
+            }
+        }
+
+    }
+}
