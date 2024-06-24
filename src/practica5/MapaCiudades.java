8bbf3a66f7ee25ac78828e2bfe00287225257879
lucianodev <mirendaluciano@gmail.com>
2024-06-24 18:33:57 -0300

    devolverCaminos

--- src/practica5/MapaCiudades.java
+++ src/practica5/MapaCiudades.java
@@ -1,6 +1,7 @@
 package practica5;
 import practica5.adjList.AdjListGraph;
 
+import javax.net.ssl.SSLContext;
 import java.util.*;
 public class MapaCiudades {
 
@@ -11,41 +12,78 @@ public class MapaCiudades {
     }
     public List<String> devolverCamino(String ciudad1, String ciudad2) {
         boolean[] visitados = new boolean[grafo.getSize()];
-        List<String> caminoActual = new ArrayList<>();
-        List<String> caminoFinal = new ArrayList<>();
-
-        for (int i = 0; i < grafo.getSize(); i++) {
-            if (ciudad1.equals(grafo.getVertex(i).getData())) {
-                visitados[i] = true;
-                caminoActual.add(grafo.getVertex(i).getData());
-                devolverCamino(grafo, i, visitados, caminoActual,caminoFinal,ciudad2);
-            }
-
+        List<String> camino = new ArrayList<>();
+        Vertex<String> origen = this.grafo.search(ciudad1);
+        Vertex<String> destino = this.grafo.search(ciudad2);
+        if (origen != null && destino != null) {
+            devolverCamino(origen,destino,camino,visitados);
         }
 
-        return caminoFinal;
+        return camino;
     }
 
-    private void devolverCamino(Graph<String> grafo, Integer i, boolean[] visitados, List<String> caminoActual,List<String> caminoFinal, String ciudad2) {
-        int j;
-        Vertex<String> v = grafo.getVertex(i);
-        for(Edge<String> e : grafo.getEdges(v)){
-            j = e.getTarget().getPosition();
-            if (!visitados[j]) {
-                visitados[j] = true;
-                caminoActual.add(e.getTarget().getData());
-                if (ciudad2.equals(e.getTarget().getData())) {
-                    caminoFinal.addAll(caminoActual);
-                } else {
-                    devolverCamino(grafo, j, visitados, caminoActual, caminoFinal, ciudad2);
+    private boolean devolverCamino(Vertex<String> origen, Vertex<String> destino, List<String> camino, boolean[] visitados) {
+        boolean encontre = false;
+        visitados[origen.getPosition()] = true;
+        camino.add(origen.getData());
+        if (origen == destino) {
+            encontre = true;
+        }  else {
+            List<Edge<String>> ady = this.grafo.getEdges(origen);
+            Iterator<Edge<String>> it = ady.iterator();
+            while (it.hasNext() && !encontre) {
+                Vertex<String> v = it.next().getTarget();
+                int j = v.getPosition();
+                if (!visitados[j]) {
+                    encontre = devolverCamino(v, destino, camino, visitados);
                 }
-                caminoActual.remove(caminoActual.size() - 1);
             }
         }
+        if (!encontre) {
+            camino.remove(camino.size() - 1);
+        }
+        return encontre;
+    }
 
+    public List<String> devolverCaminoExceptuando(String ciudad1, String ciudad2, List<String> ciudades) {
+        List<String> lista = new ArrayList<>();
+        if (!this.grafo.isEmpty()) {
+            boolean[] visitados = new boolean[this.grafo.getSize()];
+            Vertex<String> origen = this.grafo.search(ciudad1);
+            Vertex<String> destino = this.grafo.search(ciudad2);
+            if (origen != null && destino != null) {
+                devolverCaminoExceptuando(origen, destino, visitados, ciudades, lista);
+            }
+
+        }
+        return lista;
     }
 
 
+    private boolean devolverCaminoExceptuando(Vertex<String> origen,Vertex<String> destino,boolean[] visitados,
+                                              List<String> ciudades,List<String> lista) {
+        boolean encontre = false;
+        visitados[origen.getPosition()] = true;
+        lista.add(origen.getData());
+        if (origen == destino) {
+            encontre = true;
+        } else {
+            List<Edge<String>> ady = this.grafo.getEdges(origen);
+            Iterator<Edge<String>> it = ady.iterator();
+            while (it.hasNext() && !encontre) {
+                Vertex<String> v = it.next().getTarget();
+                int j = v.getPosition();
+                if (!visitados[j] && !ciudades.contains(v.getData())) {
+                    encontre = devolverCaminoExceptuando(v, destino, visitados, ciudades, lista);
+                }
+            }
+        }
+        if (!encontre) {
+            lista.remove(lista.size()-1);
+        }
+
+        return encontre;
+    }
 
 
 
--- src/practica5/TestMapaCiudades.java
+++ src/practica5/TestMapaCiudades.java
@@ -1,5 +1,6 @@
 package practica5;
-
+import java.util.ArrayList;
+import java.util.List;
 public class TestMapaCiudades {
 
     public static void main(String[] args) {
@@ -10,12 +11,21 @@ public class TestMapaCiudades {
         Vertex<String> v3 = mapa.grafo.createVertex("Roca");
         Vertex<String> v4 = mapa.grafo.createVertex("Neuquen");
         Vertex<String> v5 = mapa.grafo.createVertex("Buenos Aires");
+        Vertex<String> v6 = mapa.grafo.createVertex("Bariloche");
 
         mapa.grafo.connect(v1,v2);
         mapa.grafo.connect(v2,v3);
-        mapa.grafo.connect(v2,v5);
+        mapa.grafo.connect(v3,v4);
+        mapa.grafo.connect(v2,v6);
+        mapa.grafo.connect(v6,v4);
+        mapa.grafo.connect(v1, v5);
         mapa.grafo.connect(v5,v4);
 
-        System.out.println(mapa.devolverCamino("Cipolletti","Neuquen"));
+        List<String> ciudades = new ArrayList<>();
+        ciudades.add("Bariloche");
+        ciudades.add("Roca");
+
+        System.out.println(mapa.devolverCaminoExceptuando("Cipolletti","Neuquen",ciudades));
+
     }
 }
